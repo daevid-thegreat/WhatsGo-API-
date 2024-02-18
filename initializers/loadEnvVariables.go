9bdf2278a9228d20f69e1385dcda29d9f6b40af5
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-02-18 12:13:45 +0100

    trying to fix "Error loading .env file" error

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -8,7 +8,7 @@ import (
 
 func LoadEnvVariables() {
 	// Load .env file
-	pathDir, err := filepath.Abs(filepath.Dir(".env"))
+	pathDir, err := filepath.Abs(filepath.Dir("."))
 	err = godotenv.Load(filepath.Join(pathDir, ".env"))
 	if err != nil {
 		log.Fatal("Error loading .env file")
