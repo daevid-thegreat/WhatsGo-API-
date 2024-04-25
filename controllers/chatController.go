04cd374d2adfeff5d5a7faf029442ee6d6dbd75d
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-04-25 22:57:25 +0100

    working on chat...

--- controllers/chatController.go
+++ controllers/chatController.go
@@ -1,1 +1,20 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func OpenChat(c *gin.Context) {
+	// Get the user email from the request body
+	var body struct {
+		Email string
+	}
+
+	if c.BindJSON(&body) != nil {
+		c.JSON(400, gin.H{"error": "Fields are empty or not valid"})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "User created successfully"})
+
+}
