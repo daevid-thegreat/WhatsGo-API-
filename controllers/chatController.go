411a6db3b6f1ae009f7cf018e91edfff5380ed0f
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-04-29 17:38:58 +0100

    working on chat...

--- controllers/chatController.go
+++ controllers/chatController.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"whatsgo/initializers"
+	"whatsgo/models"
 )
 
 func OpenChat(c *gin.Context) {
-	// Get the user email from the request body
+	// Get the other user email from the request body
 	var body struct {
 		Email string
 	}
@@ -14,6 +16,22 @@ func OpenChat(c *gin.Context) {
 		c.JSON(400, gin.H{"error": "Fields are empty or not valid"})
 		return
 	}
+
+	var user models.User
+	if err := initializers.DB.Where("email = ?", body.Email).First(&user).Error; err != nil {
+		c.JSON(400, gin.H{"error": "No account found"})
+		return
+	}
+
+	// Get the logged in user
+	var loggedInUser models.User
+	if err := initializers.DB.Where("email = ?", c.GetString("email")).First(&loggedInUser).Error; err != nil {
+		c.JSON(400, gin.H{"error": "No account logged in"})
+		return
+
+	}
+
+	// Create chat
 
 	c.JSON(200, gin.H{"message": "User created successfully"})
 
